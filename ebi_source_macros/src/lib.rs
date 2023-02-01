bdd0a6bd4c98a9839208125fdebdd0417df7e6de
Jansen <jsd.alcantara@gmail.com>
2023-01-31 20:44:44 -0400

    moved away all external deps

--- ebi_source_macros/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use proc_macro::TokenStream;
-
-mod abi;
-
-#[proc_macro_attribute]
-pub fn ebi_plugin(args: TokenStream, input: TokenStream) -> TokenStream {
-    abi::gen_ebi_plugin(args, input)
-}
